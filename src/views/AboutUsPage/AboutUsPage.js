f6e8e19cc3e2f0ca841a1afeb7921a8a761b48e9
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-05-24 18:41:54 +0300

    the functionality of the about us page is working properly

--- src/views/AboutUsPage/AboutUsPage.js
+++ src/views/AboutUsPage/AboutUsPage.js
@@ -17,6 +17,10 @@ import Ameen from '../../assets/profile/ameen.jpg';
 import Jaradat from '../../assets/profile/jaradat.jpg';
 import Rula from '../../assets/profile/rula.jpg';
 import Tamimi from '../../assets/profile/tamimi.jpg';
+import GitHubIcon from '../../assets/icons/github.svg';
+import LinkedIn from '../../assets/icons/linked.svg';
+import Facebook from '../../assets/icons/facebook.svg';
+import Twitter from '../../assets/icons/twitter.svg';
 
 export class AboutUsPage extends Component {
   constructor(props) {
@@ -24,9 +28,17 @@ export class AboutUsPage extends Component {
     this.state = {
       loading: true,
       showModal: false,
+      showMemberModal: false,
       serviceName: '',
       serviceImage: '',
-      serviceDescription: ''
+      serviceDescription: '',
+      memberName: '',
+      memberImage: '',
+      memberMassege: '',
+      gitHubLink: '',
+      linkedInLink: '',
+      faceBookLink: '',
+      twiterLink: ''
     };
 
     this.handleOpenModal = this.handleOpenModal.bind(this);
@@ -35,6 +47,15 @@ export class AboutUsPage extends Component {
     this.getMovieServive = this.getMovieServive.bind(this);
     this.getBooksServive = this.getBooksServive.bind(this);
     this.getArtServive = this.getArtServive.bind(this);
+    // team information
+    this.handleOpenModalMember = this.handleOpenModalMember.bind(this);
+    this.handleCloseModalMember = this.handleCloseModalMember.bind(this);
+    this.getAbukhurmaData = this.getAbukhurmaData.bind(this);
+    this.getAbumatterData = this.getAbumatterData.bind(this);
+    this.getAmmenData = this.getAmmenData.bind(this);
+    this.getJaradatData = this.getJaradatData.bind(this);
+    this.getRulaData = this.getRulaData.bind(this);
+    this.getTamimiData = this.getTamimiData.bind(this);
   }
 
   componentDidMount() {
@@ -62,7 +83,7 @@ export class AboutUsPage extends Component {
     this.setState({
       serviceName: 'News',
       serviceImage: MoviesImage,
-      serviceDescription: 'AN ADVICE: FORGET ABOUT NEWS, ITS CAUSING HEADACHE'
+      serviceDescription: 'Check out the latest news around the world and catch up with all the breaking news.'
     });
     this.handleOpenModal();
   }
@@ -71,7 +92,7 @@ export class AboutUsPage extends Component {
     this.setState({
       serviceName: 'Movies',
       serviceImage: MoviesImage,
-      serviceDescription: 'AN ADVICE: DONT WASTE A LOT OF TIME WATCHIMG MOVIES'
+      serviceDescription: 'Bring all of your friends and get ready for \'movie night\' that you will never forget. '
     });
     this.handleOpenModal();
   }
@@ -80,7 +101,7 @@ export class AboutUsPage extends Component {
     this.setState({
       serviceName: 'Books',
       serviceImage: MoviesImage,
-      serviceDescription: 'AN ADVICE: YOU READ MORE BOOKS YOU GET MORE INFORMATION'
+      serviceDescription: 'Discove new adventure and soar away with your imagination and land in new unfamiliar life roles.'
     });
     this.handleOpenModal();
   }
@@ -89,10 +110,109 @@ export class AboutUsPage extends Component {
     this.setState({
       serviceName: 'Art',
       serviceImage: MoviesImage,
-      serviceDescription: 'AN ADVICE: FOR ME ARTS IT JUST A WASTE OF TIME BECAUSE I DONT UNDERSTAND THEM'
+      serviceDescription: 'Indulge your senses with extraordinary taste and style of in our special collection of artwork.'
     });
     this.handleOpenModal();
   }
+  // taem data functions
+  handleOpenModalMember = () => {
+    this.setState({
+      showMemberModal: true,
+    });
+  }
+
+  handleCloseModalMember = () => {
+    this.setState({
+      showMemberModal: false,
+      memberName: '',
+      memberImage: '',
+      memberMassege: ''
+    });
+  }
+
+  getAbukhurmaData = () => {
+    this.setState({
+      memberName: 'Abdallah Abu Khurma',
+      memberImage: Abukhurma,
+      memberMassege: 'Hey ! my name is Abdallah Abu Khurma and I studied civil engineering in Near East University in Cyprus and Im a full stack developer.',
+      gitHubLink: 'https://github.com/AbdallahAbuKhurma',
+      linkedInLink: 'https://www.linkedin.com/in/abdallah-abu-khurma-460192209/',
+      faceBookLink: 'https://web.facebook.com/abdulla.abukharma.3',
+      twiterLink: ''
+    });
+    this.handleOpenModalMember();
+  }
+
+  getAbumatterData = () => {
+    this.setState({
+      memberName: 'Mohammad Abu Matter',
+      memberImage: Abumatter,
+      memberMassege: 'My name is Mohammad Abu Mattar, Im a 👨‍💻 Software Engineer. I graduated in 2018 from Al-Isra university, I 🔧 build stuff for 🌐 the web.',
+      gitHubLink: 'https://github.com/MKAbuMattar',
+      linkedInLink: '',
+      faceBookLink: '',
+      twiterLink: ''
+    });
+    this.handleOpenModalMember();
+  }
+
+  getAmmenData = () => {
+    this.setState({
+      memberName: 'Ameen Saleh',
+      memberImage: Ameen,
+      memberMassege: 'Extremely motivated to constantly develop my skills and grow professionally. I am confident in my ability to come up with interesting ideas for unforgettable marketing campaigns',
+      gitHubLink: 'https://github.com/AmeenBassamSaleh',
+      linkedInLink: 'https://www.linkedin.com/in/ameenbetawi/',
+      faceBookLink: 'https://www.facebook.com/Ameenbetawi',
+      twiterLink: 'https://twitter.com/ameenbassam111?fbclid=IwAR1CLTsq4KEKdkRohZ55T0trqCfiBvy1baoTW7K0sJtajPkNiSYYGi95P90'
+    });
+    this.handleOpenModalMember();
+  }
+
+  getJaradatData = () => {
+    this.setState({
+      memberName: 'Mohammad Jaradat',
+      memberImage: Jaradat,
+      memberMassege: 'My name is Mohammad Jaradat. I have a bachelors degree in chemistry from Yarmouk University . I am  interested in this course because I want to develop my skills.',
+      gitHubLink: 'https://github.com/morjaradat/react-app',
+      linkedInLink: 'https://www.linkedin.com/in/mohammad-jaradat-977305209/',
+      faceBookLink: 'https://web.facebook.com/mjradat3/',
+      twiterLink: ''
+    });
+    this.handleOpenModalMember();
+  }
+
+  getRulaData = () => {
+    this.setState({
+      memberName: 'Rula ALqasem',
+      memberImage: Rula,
+      memberMassege: 'Im a biomedical engineer graduate in 2020 from JUST and I am looking forward to improving my ability in programming.',
+      gitHubLink: 'https://github.com/RulaAlqasem',
+      linkedInLink: '',
+      faceBookLink: '',
+      twiterLink: ''
+    });
+    this.handleOpenModalMember();
+  }
+
+  getTamimiData = () => {
+    this.setState({
+      memberName: 'Mohammad ALtamimi',
+      memberImage: Tamimi,
+      memberMassege: 'Hey ! My name is Mohammad Altamimi, I am a mechanical engineer and my goal is to kick-start a career path in software development.',
+      gitHubLink: 'https://github.com/MohammadAltamimi98',
+      linkedInLink: 'https://www.linkedin.com/in/mohammad-mech-eng',
+      faceBookLink: 'https://web.facebook.com/m98altamimi',
+      twiterLink: 'https://twitter.com/MuhammadT98'
+    });
+    this.handleOpenModalMember();
+  }
+
+  link = (e) => {
+    console.log(e);
+    window.open(e);
+  }
+
 
   render() {
     return (
@@ -127,12 +247,28 @@ export class AboutUsPage extends Component {
             </ReactModal>
             <hr />
             <h1>OUR TEAM</h1>
-            <img src={Abukhurma} alt='Abukhurma' />
-            <img src={Abumatter} alt='Abumatter' />
-            <img src={Ameen} alt='Ameen' />
-            <img src={Jaradat} alt='Jaradat' />
-            <img src={Rula} alt='Rula' />
-            <img src={Tamimi} alt='Tamimi' />
+            <img onClick={this.getAbukhurmaData} src={Abukhurma} alt='Abukhurma' />
+            <img onClick={this.getAbumatterData} src={Abumatter} alt='Abumatter' />
+            <img onClick={this.getAmmenData} src={Ameen} alt='Ameen' />
+            <img onClick={this.getJaradatData} src={Jaradat} alt='Jaradat' />
+            <img onClick={this.getRulaData} src={Rula} alt='Rula' />
+            <img onClick={this.getTamimiData} src={Tamimi} alt='Tamimi' />
+            <hr />
+            <ReactModal
+              isOpen={this.state.showMemberModal}
+              contentLabel="Minimal Modal Example"
+            >
+              <button onClick={this.handleCloseModalMember}>Close Modal</button>
+              <h1>{this.state.memberName}</h1>
+              <img src={this.state.memberImage} alt={this.state.memberImage} />
+              <p>{this.state.memberMassege}</p>
+              <img onClick={() => this.link(this.state.faceBookLink)} src={Facebook} alt='Facebook' />
+              <img onClick={() => this.link(this.state.gitHubLink)} src={GitHubIcon} alt='GitHubIcon' />
+              <img onClick={() => this.link(this.state.linkedInLink)} src={LinkedIn} alt='LinkedIn' />
+              {this.state.twiterLink !== '' &&
+                <img onClick={() => this.link(this.state.twiterLink)} src={Twitter} alt='Twitter' />
+              }
+            </ReactModal>
           </>
         )
         }
